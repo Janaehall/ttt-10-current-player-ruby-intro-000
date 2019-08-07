39e416450b19d8853413294a315a0fa8dbebe290
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-08-07 01:28:02 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,17 @@
 def turn_count(board)
   counter = 0
   board.each do |index|
+    counter += 1
     if index.upcase == "X" or index.upcase == "O"
-      counter += 1
     end
   end
   counter
+end
+
+def current_player (count)
+  if count % 2 == 0
+    "o"
+  else
+    "x"
+  end
 end
